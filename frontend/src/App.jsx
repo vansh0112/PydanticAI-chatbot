b5cddc1554fbc89eba5e04d497dc407158b3d251
vansh0112 <107179070+vansh0112@users.noreply.github.com>
2025-07-07 20:31:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId="419802293815-q2fip29gsq6pp4d2e20itms3dualjbnu.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId=ENTER YOUR CLIENT ID>
       <AuthProvider>
         <ChatUI />
       </AuthProvider>
